9befd9a81349bc9531ddd5b588ccecbedf226db9
jptolozac <jeanpaultoloza@gmail.com>
2026-04-20 22:54:27 -0500

    Initialize Spring Boot project with Gradle setup, PostgreSQL configuration, and basic application structure

---
+++ src/main/java/com/acm/ecommerce/controllers/initController.java
@@ -0,0 +1,12 @@
+package com.acm.ecommerce.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/")
+public class initController {
+    @GetMapping("/")
+    public String hello() {
+        return "hello dirty world";
+    }
+}
