672668eee2dd95aa28fbd761e5061ef17f6dd091
jairetsell <leonjairetsell@gmail.com>
2026-04-21 21:39:50 -0500

    feat: jpa persistence dependencies

--- src/main/java/com/acm/ecommerce/entities/ProductEntity.java
+++ src/main/java/com/acm/ecommerce/entities/ProductEntity.java
@@ -1,4 +1,15 @@
 package com.acm.ecommerce.entities;
 
+
+import jakarta.persistence.Entity;
+import org.hibernate.annotations.SQLDelete;
+import org.springframework.data.relational.core.mapping.Table;
+
+@Entity
+@Table(name = "products")
+@SQLDelete(sql = "UPDATE products SET deleted = true WHERE id=?")
+
+
 public class ProductEntity {
+
 }
