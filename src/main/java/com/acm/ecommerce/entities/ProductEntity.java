648ca3e02336ea01d7308bd20eb499fc65a91ff9
jptolozac <jeanpaultoloza@gmail.com>
2026-04-22 05:07:01 -0500

    feat: add MapStruct mapper for ProductEntity and update dependencies

--- src/main/java/com/acm/ecommerce/entities/ProductEntity.java
+++ src/main/java/com/acm/ecommerce/entities/ProductEntity.java
@@ -2,13 +2,15 @@ package com.acm.ecommerce.entities;
 
 
 import jakarta.persistence.*;
+import lombok.Data;
 import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "products")
-@SQLDelete(sql = "UPDATE products SET deleted = true WHERE id=?")
-
-
+@SQLDelete(sql = "UPDATE products SET deleted = true WHERE id=? AND version=?")
+@SQLRestriction("deleted = false")
+@Data
 public class ProductEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,60 +24,4 @@ public class ProductEntity {
     @Version
     private Long version;
     private boolean deleted = Boolean.FALSE;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public Integer getStock() {
-        return stock;
-    }
-
-    public void setStock(Integer stock) {
-        this.stock = stock;
-    }
-
-    public Long getVersion() {
-        return version;
-    }
-
-    public void setVersion(Long version) {
-        this.version = version;
-    }
-
-    public boolean isDeleted() {
-        return deleted;
-    }
-
-    public void setDeleted(boolean deleted) {
-        this.deleted = deleted;
-    }
 }
